c85dcb6570760fb1fa380439df5d0b2b80fe9736
Nick Raienko <enaqxx@gmail.com>
2015-08-07 22:07:13 +0300

    Change Boolean tests style to ES2015

--- test/booleans.js
+++ test/booleans.js
@@ -6,32 +6,32 @@
 import * as assert from 'assert';
 import { pragmaBooleans } from '../pragma';
 
-describe('Booleans', function() {
+describe('Booleans', () => {
 
   /* Built-in */
-  describe('#toString()', function() {
-    it('should return string according to boolean value', function() {
+  describe('#toString()', () => {
+    it('should return string according to boolean value', () => {
       assert.deepEqual((true).toString(), 'true');
       assert.deepEqual((false).toString(), 'false');
     });
   });
 
-  describe('#toNumber()', function() {
-    it('should return number according to boolean value', function() {
+  describe('#toNumber()', () => {
+    it('should return number according to boolean value', () => {
       assert.equal((true).toNumber(), 1);
       assert.equal((false).toNumber(), 0);
     });
   });
 
-  describe('#toWord()', function() {
-    it('should return "yes" or "no" depending on boolean value', function() {
+  describe('#toWord()', () => {
+    it('should return "yes" or "no" depending on boolean value', () => {
       assert.deepEqual((true).toWord(), 'yes');
       assert.deepEqual((false).toWord(), 'no');
     });
   });
 
-  describe('#opposite()', function() {
-    it('should opposite value of boolean', function() {
+  describe('#opposite()', () => {
+    it('should opposite value of boolean', () => {
       assert.equal((true).opposite(), false);
       assert.equal((false).opposite(), true);
     });
