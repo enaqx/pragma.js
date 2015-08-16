ba42c3d44ce3b939f4fdec5d0a97ec48cc159649
Nick Raienko <enaqxx@gmail.com>
2015-08-16 22:20:01 +0300

    Add Set.prototype.add() method to set test

--- test/sets.js
+++ test/sets.js
@@ -2,32 +2,39 @@
  * Sets tests
  */
 
- import * as assert from 'assert';
- import { pragmaSets } from '../pragma';
+import * as assert from 'assert';
+import { pragmaSets } from '../pragma';
 
- describe.only('Sets', () => {
+describe.only('Sets', () => {
 
-   /* Built-in */
-   describe('#size', () => {
-     it('is the size of set', () => {
-       assert.equal(new Set([1, 2, 3]).size, 3);
-       assert.equal(new Set(['a', 'b', 'c', 'd']).size, 4);
-     });
-   });
-
-
-   describe('#getLength()', () => {
-     it('should return set length', () => {
-       assert.equal(new Set([1, 2, 3]).getLength(), 3);
-       assert.equal(new Set(['a', 'b', 'c', 'd']).getLength(), 4);
-     });
-   });
-
-   describe('#toArray()', () => {
-    it('should convert set to array', () => {
-      let set = new Set(['foo', 'bar']);
-      assert.deepEqual(set.toArray(), ['foo', 'bar']);
+  /* Built-in */
+  describe('#size', () => {
+    it('is the size of set', () => {
+      assert.equal(new Set([1, 2, 3]).size, 3);
+      assert.equal(new Set(['a', 'b', 'c', 'd']).size, 4);
     });
-   });
+  });
 
- });
+  /* Built-in */
+  describe('#add()', () => {
+    it('should appends a new element to a set', () => {
+      assert.deepEqual(new Set().add(1).add(2).add(3).toArray(), [1, 2, 3]);
+    });
+  });
+
+
+  describe('#getLength()', () => {
+    it('should return set length', () => {
+      assert.equal(new Set([1, 2, 3]).getLength(), 3);
+      assert.equal(new Set(['a', 'b', 'c', 'd']).getLength(), 4);
+    });
+  });
+
+
+  describe('#toArray()', () => {
+    it('should convert set to array', () => {
+      assert.deepEqual(new Set(['foo', 'bar']).toArray(), ['foo', 'bar']);
+    });
+  });
+
+});
