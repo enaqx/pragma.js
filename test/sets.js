5ba2a3d3c21b9ca22dd67b3bf5d3e11d03da8abb
Nick Raienko <enaqxx@gmail.com>
2015-08-16 22:19:01 +0300

    Add getLength() method to Set

--- test/sets.js
+++ test/sets.js
@@ -5,7 +5,23 @@
  import * as assert from 'assert';
  import { pragmaSets } from '../pragma';
 
- describe('Sets', () => {
+ describe.only('Sets', () => {
+
+   /* Built-in */
+   describe('#size', () => {
+     it('is the size of set', () => {
+       assert.equal(new Set([1, 2, 3]).size, 3);
+       assert.equal(new Set(['a', 'b', 'c', 'd']).size, 4);
+     });
+   });
+
+
+   describe('#getLength()', () => {
+     it('should return set length', () => {
+       assert.equal(new Set([1, 2, 3]).getLength(), 3);
+       assert.equal(new Set(['a', 'b', 'c', 'd']).getLength(), 4);
+     });
+   });
 
    describe('#toArray()', () => {
     it('should convert set to array', () => {
