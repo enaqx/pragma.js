b8ebe3cae7ce80f13ac7f436c3511087de432365
Nick Raienko <enaqxx@gmail.com>
2015-08-16 22:24:10 +0300

    Add Set.prototype.addValues() method

--- test/objects.js
+++ test/objects.js
@@ -7,10 +7,4 @@ import { pragmaObjects } from '../pragma';
 
 describe('Objects', () => {
 
-  describe('#values()', () => {
-    it('should return array ob object values', () => {
-      assert.deepEqual({'foo': 1, 'bar': 2}.values(), [1, 2]);
-    });
-  });
-
 });
