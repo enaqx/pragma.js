5227cd7b072fe62b0d6178b2b8060e1ab074a7a4
Nick Raienko <enaqxx@gmail.com>
2016-01-24 16:31:29 +0200

    Add Date tests

--- doc/LINKS.md
+++ doc/LINKS.md
@@ -1,2 +1,6 @@
 Enums:
  * https://github.com/adrai/enum
+
+Dates:
+ * https://github.com/moment/moment
+ * https://github.com/rauchg/ms.js
---
+++ test/dates.js
@@ -0,0 +1,26 @@
+/**
+  * Date tests
+  */
+
+import { assert } from 'chai';
+
+
+describe('Dates', () => {
+
+  /**
+    * Built-in methods
+    */
+
+  describe('#now()', () => {
+    it('returns the number of milliseconds since Unix time', () => {
+      assert.isNaN(Date.UTC(2016));
+      assert.equal(Date.UTC(2016, 1), 1454284800000);
+      assert.equal(Date.UTC(2016, 1, 2, 3), 1454382000000);
+      assert.equal(Date.UTC(2016, 1, 2, 3, 4), 1454382240000);
+      assert.equal(Date.UTC(2016, 1, 2, 3, 4, 5), 1454382245000);
+      assert.equal(Date.UTC(2016, 1, 2, 3, 4, 5, 6), 1454382245006);
+      assert.equal(Date.UTC(2016, 1, 2, 3, 4, 5, 6, 7), 1454382245006);
+    });
+  });
+
+});
